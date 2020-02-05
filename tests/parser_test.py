2317176e42be90c166a1938fc4ff04d77fcfb8bb
Yang Yang <yyang.even@gmail.com>
2020-02-05 12:50:02 -0800

    Tests: Split data entry tests to parser_to_data_entry_test.py
    Change-Id: I66380a3feb02d30514bcce1cb50a57e281b2faa6

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -8,7 +8,6 @@ import unittest
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 
 import constants
-from mobile_code_data import MobileCodeDataEntry
 import parser
 
 
@@ -63,97 +62,6 @@ class TestExtractAllMobileCodeTables(unittest.TestCase):
                 )
             )
             self.assertEqual(constants.TABLE_SIZES_OF_MAIN, all_table_lengths)
-
-
-class TestToDataEntry(unittest.TestCase):
-    def setUp(self):
-        self.HEADERS = [
-            "mcc",
-            "mnc",
-            "brand",
-            "operator",
-            "status",
-            "bands",
-            "references_and_notes",
-        ]
-
-    def test_DataOneLessThanHeaders(self):
-        data_row_tag = parser.MakeSoup(
-            """
-<tr>
-<td>001</td>
-<td>01</td>
-<td>TEST</td>
-<td>Test Network</td>
-<td>Operational</td>
-<td>any</td>
-<td>
-</td></tr>"""
-        )
-        self.assertEqual(
-            MobileCodeDataEntry(
-                mcc="001",
-                mnc="01",
-                brand="TEST",
-                operator="Test Network",
-                status="Operational",
-                bands="any",
-                references_and_notes="",
-            ),
-            parser.ToDataEntry(data_row_tag, self.HEADERS),
-        )
-
-    def test_EmptyBrand(self):
-        data_row_tag = parser.MakeSoup(
-            """
-<tr>
-<td>901</td>
-<td>02</td>
-<td></td>
-<td><i>Unassigned</i></td>
-<td>Returned spare</td>
-<td>Unknown</td>
-<td>Formerly: Sense Communications International
-</td></tr>"""
-        )
-        self.assertEqual(
-            MobileCodeDataEntry(
-                mcc="901",
-                mnc="02",
-                brand="",
-                operator="Unassigned",
-                status="Returned spare",
-                bands="Unknown",
-                references_and_notes="Formerly: Sense Communications International",
-            ),
-            parser.ToDataEntry(data_row_tag, self.HEADERS),
-        )
-
-    def test_ContainsLinks(self):
-        data_row_tag = parser.MakeSoup(
-            """
-<tr>
-<td>901</td>
-<td>01</td>
-<td>ICO</td>
-<td><a href="/wiki/ICO_Satellite_Management" class="mw-redirect" title="ICO Satellite Management">ICO Satellite Management</a></td>
-<td>Not operational</td>
-<td>Satellite</td>
-<td>MNC withdrawn<sup id="cite_ref-itu_ob_1091_5-0" class="reference"><a href="#cite_note-itu_ob_1091-5">[5]</a></sup>
-</td></tr>"""
-        )
-        self.assertEqual(
-            MobileCodeDataEntry(
-                mcc="901",
-                mnc="01",
-                brand="ICO",
-                operator="ICO Satellite Management",
-                status="Not operational",
-                bands="Satellite",
-                references_and_notes="MNC withdrawn[5]",
-            ),
-            parser.ToDataEntry(data_row_tag, self.HEADERS),
-        )
 
 
 if __name__ == "__main__":
